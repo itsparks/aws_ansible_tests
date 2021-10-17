51c3db0242c8d9a221ae5f12c9964b31216d8ed5
Scott Parks <>
2021-10-16 22:12:39 -0500

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "ansible_master" {
+  ami = "ami-02e136e904f3da870"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "ansible_master"
+  }
+}
